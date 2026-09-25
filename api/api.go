b4bc6af72548f0b4265204bcfc381587d70ac4d6
mchipperfield <189554036+mchipperfield@users.noreply.github.com>
2026-09-25 14:06:40 +0100

    update mux routing

--- api/api.go
+++ api/api.go
@@ -33,8 +33,8 @@ func NewHandler(logger *slog.Logger, svc blog.Service) (*Handler, error) {
 		svc:    svc,
 	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /blog", h.GetArticles())
-	mux.HandleFunc("GET /blog/{slug}", h.GetArticleBySlug())
+	mux.HandleFunc("GET /", h.GetArticles())
+	mux.HandleFunc("GET /{slug}", h.GetArticleBySlug())
 	h.Handler = h.LoggingMw(mux)
 	h.Handler = otelhttp.NewHandler(h.Handler, "blog-handler")
 	return &h, nil
